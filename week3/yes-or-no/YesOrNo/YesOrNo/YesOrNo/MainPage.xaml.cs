1d0619106b551bdc690c3962d42d1d8df538b99f
Erica1217 <ericakim@sowrender.com>
2017-02-16 10:08:57 +0900

    Upload files

---
+++ week3/yes-or-no/YesOrNo/YesOrNo/YesOrNo/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace YesOrNo
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        async void EntryClicked(object sender, EventArgs args)
+        {
+            await Navigation.PushModalAsync(new Response());
+        }
+    }
+}
---
+++ week3/yes-or-no/YesOrNo/YesOrNo/YesOrNo/Response.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace YesOrNo
+{
+    public partial class Response : ContentPage
+    {
+        public Response()
+        {
+            InitializeComponent();
+        }
+
+
+        async void ExitClicked(object sender, EventArgs args)
+        {
+            await Navigation.PopAsync();
+        }
+
+        void NoClicked(object sender, EventArgs args)
+        {
+            //YesButton.SetValue("BackgroundColor", "#ffffff");
+            //((Button) YesButton).
+            //NoButton.BackgroundColor = Color.Red;
+        }
+        void YesClicked(object sender, EventArgs args)
+        {
+            //NoButton.BackgroundColor = Color.White;
+            //YesButton.BackgroundColor = Color.Green;
+        }
+    }
+}
